17dae87626bf51ede6e73ba314035f4f3c8d6253
Bilalnoori7 <152948220+Bilalnoori7@users.noreply.github.com>
2025-06-09 16:22:38 -0400

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,75 +1,92 @@
-More actions
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_cust_info;
+/*
+===============================================================================
+Create Bronze Tables
+===============================================================================
+What this does:
+   This script makes new tables in the 'bronze' schema. If the tables already 
+   exist, it deletes them first and then makes new ones.
+   Run this to set up the bronze tables.
+===============================================================================
+*/
+
+-- A ddl creates, modifies, or deletes a table in the database objects like (tables, views, etc...)
+
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL  -- Searches for a user table 'U' and drops it if exist
+    DROP TABLE bronze.crm_cust_info;
+GO
+
 CREATE TABLE bronze.crm_cust_info (
-	cst_id INT,
-	cst_key NVARCHAR(50),
-	cst_firstname NVARCHAR(50),
-	cst_lastname NVARCHAR(50),
-	cst_marital_status NVARCHAR(50),
-	cst_gender NVARCHAR(50),
-	cst_create_date DATE,
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
 );
+GO -- Tells sql to execute everything above before continuing
 
-
-IF OBJECT_ID ('bronze.crm_prd_info','U')  IS NOT NULL
-	DROP TABLE bronze.crm_prd_info;
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
 
 CREATE TABLE bronze.crm_prd_info (
-	prd_id INT,
-	prd_key NVARCHAR(50),
-	prd_nm NVARCHAR(50),
-	prd_cost INT,
-	prd_line NVARCHAR(50),
-	prd_start_dt DATETIME,
-	prd_end_dt DATETIME,
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
 );
+GO
 
-IF OBJECT_ID ('bronze.crm_sales_details','U') IS NOT NULL
-	DROP TABLE bronze.crm_sales_details;
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
 
 CREATE TABLE bronze.crm_sales_details (
-	sls_ord_num NVARCHAR(50),
-	sls_prd_key NVARCHAR(50),
-	sls_cust_id INT,
-	sls_order_date INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
 );
+GO
 
-IF OBJECT_ID ('bronze.erp_cust_az12','U') IS NOT NULL
-	DROP TABLE bronze.erp_cust_az12;
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
 
-CREATE TABLE bronze.erp_cust_az12(
-	cid NVARCHAR(50),
-	bdate INT,
-	gen NVARCHAR(50)
-
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
 );
+GO
 
-IF OBJECT_ID ('bronze.erp_loc_a101','U') IS NOT NULL
-	DROP TABLE bronze.erp_loc_a101;
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
 
-CREATE TABLE bronze.erp_loc_a101(
-	cid NVARCHAR(50),
-	cntry NVARCHAR(50)
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
 );
+GO
 
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
 
-IF OBJECT_ID ('bronze.erp_px_cat_g1v2','U') IS NOT NULL
-	DROP TABLE bronze.erp_px_cat_g1v2;
-
-CREATE TABLE bronze.erp_px_cat_g1v2(
-
-	id NVARCHAR(50),
-	cat NVARCHAR(50),
-	subcat NVARCHAR(50),
-	maintenence NVARCHAR(50)
-
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
 );
-
-
-Add comment
+GO
